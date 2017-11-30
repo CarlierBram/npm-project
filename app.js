bc6d66377468d6ccfc45e70b934def86d684a911
CarlierBram <33499397+CarlierBram@users.noreply.github.com>
2017-11-30 16:35:09 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
-//var serv2 = require('http').Server(app);
 
 
 app.get('/',function(req, res){
@@ -10,15 +9,11 @@ app.get('/',function(req, res){
 app.use('/client',express.static(__dirname + '/client'));
 
 serv.listen(2000);
-//serv2.listen(3000);
 console.log("listen on 2000");
 
 var io = require('socket.io')(serv,{});
-//var io2 = require('socket.io')(serv2,{});
-
 io.sockets.on('connection', function(socket){
     console.log("socket connection");
-    io.emit("test",{id: "test"});
     
     socket.on("links",function(data){
         console.log("linksclick " + data.id);
